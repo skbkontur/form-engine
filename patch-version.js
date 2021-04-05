c8b3803fd220f1ceea5dbf1c844c527ed57d5dee
khasanov <khasanov@skbkontur.ru>
2021-04-05 16:46:22 +0500

    fix NestedFormContainer and patch-version

--- patch-version.js
+++ patch-version.js
@@ -1,33 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const cp = require("child_process");
+const nbgv = require("nerdbank-gitversioning");
 
-function execAsync(command, options) {
-    return new Promise((resolve, reject) =>
-        cp.exec(command, options, (error, stdout, stderr) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve({ stdout: stdout, stderr: stderr });
-            }
-        })
-    );
-}
-
-async function setPackageVersion(packageDirectory) {
-    const versionText = await execAsync("dotnet nbgv get-version --variable NpmPackageVersion");
-    if (versionText.stderr) {
-        throw versionText.stderr;
-    }
-
-    const npmPackageVersion = versionText.stdout;
+async function setPackageVersion() {
+    const versionText = await nbgv.getVersion().catch(e => console.log(e));
+    const npmPackageVersion = versionText.npmPackageVersion;
     console.log(`Setting package version to ${npmPackageVersion}`);
-    const result = await execAsync(`npm version ${npmPackageVersion} --no-git-tag-version --allow-same-version`, {
-        cwd: packageDirectory,
-    });
-    if (result.stderr) {
-        console.log(result.stderr);
-    }
+    await nbgv.setPackageVersion("./dist").catch(e => console.log(e));
 }
 
-setPackageVersion("./dist");
+setPackageVersion();
